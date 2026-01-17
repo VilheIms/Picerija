a5343fff7760939df94be7dfe3d6fc9554e244d3
VilheIms <boysepticeye@gmail.com>
2026-01-17 13:32:34 +0200

    Mēģinu iesākt JFrame, vajag pārveidot visu jo nezināmu iemeslu dēļ nekas nesaglabājās

--- src/Pica.java
+++ src/Pica.java
@@ -1,4 +1,25 @@
 
-public class Pica {
-
+public class Pica extends Picerija {
+	double lielums, cena;
+	String piedevas, merce;
+	int daudzums;
+	boolean piegade;
+	
+	
+	
+	Pica(double lielums, String piedevas, String merce, boolean piegade, int daudzums, double cena){
+		
+		this.lielums = lielums;
+		this.piedevas = piedevas;
+		this.piedevas = piedevas;
+		this.piegade = piegade;
+		this.daudzums = daudzums;
+		this.cena = cena;
+		
+		
+	}
+	
+	public void start() {
+		
+	}
 }
